ddd5a75e56d2046a6c3477ffa8a6cdb7c1630924
p3rsidh <persidh.pimenta@gmail.com>
2022-10-14 15:41:54 -0300

    atualizações

--- src/main/java/com/cardinalidade/cardinalidadeJava/CardinalidadeJavaApplication.java
+++ src/main/java/com/cardinalidade/cardinalidadeJava/CardinalidadeJavaApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardinalidadeJavaApplication {
+public class
+CardinalidadeJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardinalidadeJavaApplication.class, args);
